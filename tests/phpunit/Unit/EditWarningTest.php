b1028d518522536520a06094346f3bf5010b771c
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-07-01 13:18:05 +0200

    fix sniff MediaWiki.PHPUnit.AssertEquals.Int reported by phpcs [skip ci]

--- tests/phpunit/Unit/EditWarningTest.php
+++ tests/phpunit/Unit/EditWarningTest.php
@@ -7,9 +7,9 @@ class EditWarningTest extends TestCase {
 
 	public function testConstructor() {
 		$editWarning = new EditWarning( 1, 2, 3 );
-		$this->assertEquals( 1, $editWarning->getUserID() );
-		$this->assertEquals( 2, $editWarning->getArticleID() );
-		$this->assertEquals( 3, $editWarning->getSection() );
+		$this->assertSame( 1, $editWarning->getUserID() );
+		$this->assertSame( 2, $editWarning->getArticleID() );
+		$this->assertSame( 3, $editWarning->getSection() );
 	}
 
 }
